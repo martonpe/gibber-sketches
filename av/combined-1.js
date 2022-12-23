28654510a90bafb048880bcd7a301c59f49bd10b
Peter Marton <martonpe@gmail.com>
2022-12-23 21:55:16 +0100

    Combine all sketches

---
+++ av/combined-1.js
@@ -0,0 +1,340 @@
+//SAMPLES
+moans = [];
+for (i = 0; i < 9; i++) {
+    moans[i] = "http://127.0.0.1:8080/moans/moan" + i + ".mp3"
+}
+moan =  Sampler({
+    files: moans,
+    gain: .5
+})
+choir1 = Sampler("http://127.0.0.1:8080/choir1.mp3", { gain: .5 })
+choir2 = Sampler("http://127.0.0.1:8080/choir2.mp3", { gain: .5 })
+choir3 = Sampler("http://127.0.0.1:8080/choir3.mp3", { gain: .5 })
+drone = Sampler('http://127.0.0.1:8080/drone1.mp3', { loops: true })
+losing1 = Sampler('http://127.0.0.1:8080/losing.mp3', { loops: true, gain: 1.5})
+losing2 = Sampler('http://127.0.0.1:8080/losing2.mp3', { loops: true, gain: 2})
+productive = Sampler('http://127.0.0.1:8080/technosexual/productive.mp3', { loops: true, gain: 2 })
+// the long ones
+ginsberg = Sampler('http://127.0.0.1:8080/ginsberg.mp3', { gain: 3 })
+heaven = Sampler('http://127.0.0.1:8080/heaven.mp3', { gain: 1.5 })
+ 
+ 
+//EFFECTS
+rs = Reverb({roomSize: .2} ).bus()
+r = Reverb('space').bus()
+d13 = Delay( '1/3' ).bus().connect( r, .5 )
+d38 = Delay({ time: 3/8 }).bus().connect(r, .5);
+dS = Delay({ time:1/100000, feedback: .9, wetdry: .6 })
+ 
+drone.connect(r)
+ginsberg.connect(r, .1)
+choir1.connect(r)
+choir2.connect(r)
+choir3.connect(r)
+moan.connect(d38, .6).connect(r, 1)
+ 
+ 
+//IMAGES
+genitals = [];
+for (i = 0; i < 9; i++) {
+  genitals[i] = loadImage("http://127.0.0.1:8080/geni" + i + ".png");
+}
+flowers = [];
+for (i = 0; i < 9; i++) {
+  flowers[i] = loadImage("http://127.0.0.1:8080/flower" + i + ".png");
+}
+fist = loadImage('http://127.0.0.1:8080/fist.png')
+fist2 = loadImage('http://127.0.0.1:8080/fist2.png')
+dildos = [
+    loadImage("http://127.0.0.1:8080/dildo3.png"),
+    loadImage("http://127.0.0.1:8080/dildo4.png"), 
+    loadImage("http://127.0.0.1:8080/dildo5.png"),
+    loadImage("http://127.0.0.1:8080/dildo2.png"),
+    loadImage("http://127.0.0.1:8080/dildo0.png"),
+    loadImage("http://127.0.0.1:8080/dildo1.png"),
+  ]
+   
+ 
+//HYDRA
+s0.init({ src: canvas });
+src(s0).out()
+src(s0).out(o1);
+canvas.style.display = "none";
+ 
+ 
+//FUNCTIONS & SETTINGS
+imageMode(CENTER);
+angleMode(DEGREES)
+background(0)
+resetHydra = function(){
+    src(s0).out()
+    canvas.style.display = "block";
+}
+noStroke()
+drawRays = function(w, h, x) {
+  let x0 = (width - w)/2;
+  let y0 = (height - h)/2;
+  
+  for (let i = 0; i < nDiv; i++) {
+    fill(colors[x%2]);
+    // top
+    triangle(x0 + i*w/nDiv, y0, x0 + (i+1)*w/nDiv, y0, width/2, height/2);
+    // bottom
+    triangle(x0 + i*w/nDiv, h + y0, x0 + (i+1)*w/nDiv, h + y0, width/2, height/2);
+    
+    x++;
+    fill(colors[x%2]);
+    // left
+    triangle(x0, y0 + i*h/nDiv, x0, y0 + (i+1)*h/nDiv, width/2, height/2);
+    // right
+    triangle(w + x0, y0 + i*h/nDiv, w + x0, y0 + (i+1)*h/nDiv, width/2, height/2);
+  }
+}
+scale = fibs[fibs.length-5]/fibs[fibs.length-1]
+fiboDicks = function(){
+    push()
+    for (let i = 0; i < fibs.length; i++) {
+      scaledFib = fibs[i]*scale
+      dildo = dildos[i%4]
+      
+      push()
+      rotate(-155)
+      image(dildo, -scaledFib/2, -scaledFib/2, scaledFib, scaledFib)
+      
+      rotate(-155)
+      image(dildo, scaledFib, -scaledFib/2, scaledFib, scaledFib)
+      
+      rotate(-155)
+      image(dildo, -scaledFib/2, -scaledFib/2, scaledFib, scaledFib)
+      pop()  
+      
+      translate(scaledFib, scaledFib)
+      rotate(-90)
+    }
+    pop()
+    
+    if (scale > 1) {
+      scale = fibs[fibs.length-5]/fibs[fibs.length-1]
+    } else {
+      scale *= speed
+    }
+}
+face = function(){
+    for(i=0;i<12;i++){
+      push()
+      n = map(p5.noise((frameCount+i*100)*0.008),0,1,-1,1)
+      rotate(i*30)
+      image(dildos[0],0,-200+20*n,100-20*n,100-20*n)
+      image(dildos[1],0,-270,100+30*n,100+60*n)
+      pop()
+    }
+    push()
+    rotate(120)
+    image(dildos[2],-90,-40,100,100)
+    rotate(130)
+    image(dildos[2],90,-60,100,100)
+    rotate(110)
+    image(dildos[2],10,0,100,100)
+    pop()
+    //mouth
+    push()
+    rotate(90)
+    image(dildos[4],120+heaven.out(40),0,150,150)
+    rotate(180)
+    image(dildos[4],-110+heaven.out(30),0,150,150)
+    pop()
+}
+
+
+
+//SKETCHES
+//////////////////////////////////////
+//01 ginsberg
+////////////////////////////////////
+
+
+perc = FM( 'perc').connect( d13, .5 ).connect(r,.5)
+perc.note.seq( sine( btof(7),7,7 )        , 1/2, 2 )
+
+perc.connect(r,.5)
+ginsberg.note(1)
+
+
+
+///////////////////////////////////////
+//02 yes, master
+////////////////////////////////////
+porn = loadVideo("http://127.0.0.1:8080/porn1.mp4")
+f = FM("glockenspiel", { decay: 1 / 16, gain: .7 }).connect(d13,1), connect(r,1)
+s = Sine({ frequency: 100, gain:0 })
+s.connect()
+b = FM[2]("deepbass", { attack: 0.001, decay: 1/8, gain:1 }).connect(r, 0.1);
+draw = function () {
+  image(porn, width/2, height/2, width, height);
+};
+src(o0).scale(1.005).blend(src(o1), ()=>1.5-b.out(8)).out();
+
+b.note.seq((bp = [-25]), 1/8);
+perc.stop()
+ginsberg.fx.add(dS)
+dS.time.seq([1/2, 1/4, 1/64, 1/128, 1/8, 1/1000], 2)
+
+bp.transpose.seq([1], 4)
+ginsberg.fadeout()
+
+s.frequency.fade(10,1000,32)
+s.gain.fade(0,.5,32)
+src(o0).scale(1.04).hue(0.1).blend(src(o1), ()=>0.2+b.out(10+s.out(100))).out();
+
+
+////////////////////////////////////
+//03 orgy
+////////////////////////////////////
+s.disconnect()
+perc.stop()
+b.stop()
+s = Synth("bleep").connect(r, 0.5);
+s.note.seq( (notes = [0, -3, -14, 1, -10, -4, 13, -6]), [1,1/2,1/4,1/12,1/16].rnd() );
+background(rndi(200, 255), rndi(155, 255), rndi(255));
+resetHydra()
+var i, x, y;
+draw = function () {
+  if (s.out() > 0.01 && i === undefined) {
+    background(rndi(200, 255), rndi(155, 255), rndi(255));
+    for (j = 0; j < 1; j++) {
+      i = rndi(0, 8);
+      x = rndi(width);
+      y = rndi(height);
+      push()
+      translate(x, y);
+      rotate(random(90));
+      s1 = rndi(200,400)
+      image(genitals[i], 0, 0, s1, s1);
+      pop()
+    }
+  }
+  if (s.out() < 0.01) {
+    i = undefined;
+  }
+};
+
+
+////////////////////////////////////
+//04 moaning flowers 👼🌸🌹🌼
+////////////////////////////////////
+s.stop()
+moan.connect(d38, .6).connect(r, 1)
+moan.pick.seq( Rndi(0,9) )
+moan.note.seq( [1,.75, 1.2,1.25,1.3].rnd(), [2,1.5,1,1/2,1/4,1/12,1/16].rnd() )
+background(rndi(200, 255), rndi(155, 255), rndi(255));
+var iNo;
+draw = function () {
+  if (moan.__out > 0.01 && iNo === undefined) {
+      iNo = rndi(0, 8);
+      x = rndi(width);
+      y = rndi(height);
+      s1 = rndi(200,400)
+      push()
+      translate(x, y);
+      rotate(random(90));
+      image(flowers[iNo], 0, 0, s1, s1)
+    	pop()
+  }
+  if (moan.__out < 0.01) {
+    iNo = undefined;
+  }
+};
+canvas.style.display = "none";
+src(o0).modulateHue(src(s0).rotate(() => time%1060).diff(s0),()=>moan.out(110)).blend(src(o1), .5).out()
+
+src(o0).modulateHue(src(s0).rotate(() => time%1060).diff(s0),()=>moan.out(110)).blend(src(o1), .03).out()
+
+
+////////////////////////////////////
+//05 fist the system
+////////////////////////////////////
+statik = loadVideo('http://127.0.0.1:8080/static.mp4')
+b = Monosynth("short.dry", {decay: .1});
+b.note.seq( [-20, -20, -20, -20, -21], 1/8 );
+b.loudness.seq( [1, 0.2, 0.2, 0.2, 0.2, 0.2, 0.2], 1/8 );
+
+resetHydra()
+draw = function() {
+    background(0)
+    image(statik, width/2, height/2, width, height)
+    image(fist, width/2, height/2, fist.width-200, fist.height-200)
+    //image(fist2, random(width), random(height), fist.width-200, fist.height-200)
+    rotate(90,0)
+    //image(fist, random(height), -random(width), fist.height, fist.width)
+}
+osc(6,0.03,2).out(o2);
+canvas.style.display = "none";
+src(o0).modulateHue(src(o0).scale(1.002).kaleid(10),100).blend(src(o1), ()=>1-b.out(10)).blend(src(o2),.02).out()
+
+s = Synth("bleep", { decay: 1/16, gain: .7 }).connect(r, .5)
+s.loudness.seq([1, 0.05, 0.05, 0.05, 0.05, 0.2, 0.1, 0.1], 1/16)
+s.note.seq([0, 2, 4, 5, 8], 1/16)
+
+hh = Hat({ gain: 0.1, tune: 0.7 }).connect(r, .3)
+hh.gain.seq([0.2, 0.2, 0.2, 0.2, 0.4, 0.2].rnd(), 1/16);
+hh.trigger.seq(1, 1/16);
+hh.fx.add(dS);
+dS.time = 1/10000;
+
+dS.time.seq([1/2, 1/10000, 1/4, 1/8, 1/1000, 1/128], [1,2].rnd())
+
+////////////////////////////////////
+//06 tunnel to heaven
+////////////////////////////////////
+s.stop()
+hh.stop()
+b.stop()
+resetHydra()
+nDiv = 10
+step = .05
+colors = [[255, 255, 255], [0, 0, 0]];
+draw = function () {
+    let percent = (frameCount/2000)%(2*step);
+    let x = 0;
+    for (let i = 1; i > 0; i -= step) {
+      let alpha = pow(i + percent, 5);
+      drawRays(alpha*width, alpha*height, x);
+      x++;
+    }
+    smooth = 6000
+    image(dildos[4], width/2, height/2, choir1.out(2000,1,smooth), choir1.out(2000,1,smooth))
+    image(dildos[5], width/2, height/2-choir2.out(800,0,smooth), choir2.out(2000,1,smooth), choir2.out(2000,1,smooth))
+}
+choir1.note.seq( [1,.75, .5].rnd(), Rndf(4,8) )
+choir2.note.seq( [1,.75, .5].rnd(), Rndf(4,8) )
+choir3.note.seq( [1,.75, .5].rnd(), Rndf(4,8) )
+
+colors[1] = colors[1].map((c)=>c+10)
+
+////////////////////////////////////
+//07 dildo heaven
+////////////////////////////////////
+drone.note(1)
+drone.fadein()
+speed = 1.008
+choir1.stop()
+choir2.stop()
+choir3.stop()
+s = Sine({ frequency: 1000, gain:0 }).connect()
+
+draw = function() {
+  n = map(p5.noise(frameCount*0.008),0,1,-1,1)
+  translate(width/2+100*n, height/2+100*n)
+  fiboDicks(false, true, false)
+  rotate(frameCount*0.1)
+  //face()
+}
+
+heaven.trigger(1)
+
+s.gain.fade(0, .3, 32)
+canvas.style.display = "none";
+src(o0).scale(1.0).blend(src(o1), ()=>1+s.out(40)).out();
+
+drone.fadeout()
+s.gain.fade(.3,0,16)
---
+++ av/combined-2.js
@@ -0,0 +1,539 @@
+//SAMPLES
+ambiences = [];
+for (i = 0; i < 9; i++) {
+    ambiences[i] = Sampler("http://127.0.0.1:8080/hdri/" + i + ".wav", {loops: true, gain: 0})
+}
+for (i = 0; i < 9; i++) {
+	ambiences[i].gain = 0
+	ambiences[i].note(1)
+}
+drone = Sampler('http://127.0.0.1:8080/drone1.mp3', { loops: true })
+rave = Sampler('http://127.0.0.1:8080/technosexual/rave2.mp3',  { gain: 1.5 })
+k = Sampler('http://127.0.0.1:8080/technosexual/kick1.mp3')
+spacetime = Sampler("http://127.0.0.1:8080/spacetime.mp3", { loops: true, gain: 3 });
+loop92 = Sampler('http://127.0.0.1:8080/metatron/92.wav', { gain: 3 })
+loop93 = Sampler('http://127.0.0.1:8080/metatron/93.mp3', { gain: 3 })
+technosexual = Sampler('http://127.0.0.1:8080/technosexual/technosexual.mp3', { gain: 2 })
+productive = Sampler('http://127.0.0.1:8080/technosexual/productive.mp3', { loops: true, gain: 2 })
+sex = Sampler('http://127.0.0.1:8080/technosexual/moans.mp3', { loops: true, gain: 1.5 })
+rave = Sampler('http://127.0.0.1:8080/technosexual/rave2.mp3')
+k = Sampler('http://127.0.0.1:8080/technosexual/kick1.mp3')
+acid = Sampler('http://127.0.0.1:8080/acid2.wav', {gain: 3})
+losing1 = Sampler('http://127.0.0.1:8080/losing.mp3', { loops: true, gain: 1.5})
+losing2 = Sampler('http://127.0.0.1:8080/losing2.mp3', { loops: true })
+relax = Sampler('http://127.0.0.1:8080/relax1.mp3', { gain: 3 })
+relax2 = Sampler('http://127.0.0.1:8080/relax3.mp3', { gain: 3 })
+relax3 = Sampler('http://127.0.0.1:8080/relax4.mp3', { gain: 3 })
+relax4 = Sampler('http://127.0.0.1:8080/relax5.mp3', { gain: 3 })
+// the long ones
+changes = Sampler('http://127.0.0.1:8080/changes.mp3', { gain: 2})
+ 
+ 
+//EFFECTS
+rs = Reverb({roomSize: .2} ).bus()
+r = Reverb('space').bus()
+d13 = Delay( '1/3' ).bus().connect( r, .5 )
+d38 = Delay({ time: 3/8 }).bus().connect(r, .5);
+dS = Delay({time:1/100000, feedback: .9, })
+ 
+spacetime.connect(d38, 0.8);
+drone.connect(r);
+losing2.connect(d38, .7).connect(r)
+losing1.connect(r)
+technosexual.connect(r, .3)
+productive.connect(d38, 0.1).connect(r, .3)
+ 
+ 
+//IMAGES
+genitals = [];
+for (i = 0; i < 9; i++) {
+  genitals[i] = loadImage("http://127.0.0.1:8080/geni" + i + ".png");
+}
+flowers = [];
+for (i = 0; i < 9; i++) {
+  flowers[i] = loadImage("http://127.0.0.1:8080/flower" + i + ".png");
+}
+grindr = []
+for(i=0;i<12;i++){
+  grindr[i] = loadImage("http://127.0.0.1:8080/grindr/"+i+".jpg");
+}
+smiley = loadImage("http://127.0.0.1:8080/smiley.png")
+hdri = [];
+for (i = 0; i < 9; i++) {
+  hdri[i] = loadImage("http://127.0.0.1:8080/hdri/" + i + ".jpg");
+}
+ 
+ 
+//HYDRA
+s0.init({ src: canvas });
+s1.initCam()
+src(s0).out()
+src(s0).out(o1);
+canvas.style.display = "none";
+ 
+ 
+//FUNCTIONS & SETTINGS
+imageMode(CENTER);
+background(255)
+resetHydra = function(){
+    src(s0).out()
+    canvas.style.display = "block";
+}
+noStroke()
+clouds = loadVideo("http://127.0.0.1:8080/clouds.mp4")
+draw = function () {
+  image(clouds, width/2, height/2, width, height);
+};
+pixelDensity(1) // fixes retina display offset
+pg = createGraphics(width, height);
+pg.imageMode(CENTER)
+webgl = createGraphics(width, height, WEBGL);
+webgl.noStroke()
+glitch = new Glitch()
+glitch.loadType('jpg')
+glitch.loadQuality(.3)
+glitch.errors(false)
+pix = 100
+pixelEffect = function(vid, on){
+  vid.loadPixels();
+  	color = {r: rndi(255), g: 0, b: rndi(155)}
+  	for (let i = 0; i < vid.pixels.length; i += 4) {
+    	if (vid.pixels[i] > 255 - pix) {
+      	vid.pixels[i] = color.r;
+      	vid.pixels[i+1] = color.g;
+      	vid.pixels[i+2] = color.b;
+    	}
+  	}
+  vid.updatePixels()
+}
+font = googleFont('Black Ops One')
+pg.textFont(font)
+textFont(font)
+pg.textAlign(CENTER, CENTER)
+textAlign(CENTER, CENTER)
+pg.textSize(250)
+textSize(250)
+fill(255,0,0)
+ 
+ 
+//SHADERS
+vertShader = `
+	attribute vec3 aPosition;
+	attribute vec2 aTexCoord;
+	
+	varying vec2 vTexCoord;
+	
+	void main() {
+	  vTexCoord = aTexCoord;
+	
+	  vec4 positionVec4 = vec4(aPosition, 1.0);
+	  positionVec4.xy = positionVec4.xy * 2.0 - 1.0;
+	
+	  gl_Position = positionVec4;
+	}
+`
+fragShader = `
+	#ifdef GL_ES
+	precision mediump float;
+	#endif
+	
+	uniform sampler2D tInput;
+	uniform vec2 resolution;
+	uniform float max_distort;
+	uniform vec2 iMouse;
+	
+	vec2 barrelDistortion(vec2 coord, float amt) {
+		vec2 cc = coord - iMouse;
+		float dist = dot(cc, cc);
+		return coord + cc * dist * amt;
+	}
+	
+	float sat( float t )
+	{
+		return clamp( t, 0.0, 1.0 );
+	}
+	
+	float linterp( float t ) {
+		return sat( 1.0 - abs( 2.0*t - 1.0 ) );
+	}
+	
+	float remap( float t, float a, float b ) {
+		return sat( (t - a) / (b - a) );
+	}
+	
+	vec4 spectrum_offset( float t ) {
+		vec4 ret;
+		float lo = step(t,0.5);
+		float hi = 1.0-lo;
+		float w = linterp( remap( t, 1.0/6.0, 5.0/6.0 ) );
+		ret = vec4(lo,1.0,hi, 1.) * vec4(1.0-w, w, 1.0-w, 1.);
+	
+		return pow( ret, vec4(1.0/2.2) );
+	}
+	
+	const int num_iter = 37;
+	
+	void main()
+	{	
+		vec2 uv=(gl_FragCoord.xy/resolution.xy*1.0)+.0;
+		uv.y = 1.0 - uv.y;
+		
+		float reci_num_iter_f = 1.0 / float(num_iter);
+		vec4 sumcol = vec4(0.0);
+		vec4 sumw = vec4(0.0);	
+		for ( int i=0; i<num_iter;++i )
+		{
+			float t = float(i) * reci_num_iter_f;
+			vec4 w = spectrum_offset( t );
+			sumw += w;
+			sumcol += w * texture2D( tInput, barrelDistortion(uv, .16 * max_distort*t ) );
+		}
+			
+		gl_FragColor = sumcol / sumw;
+	}
+`
+
+
+//SKETCHES
+////////////////////////////////////
+//08 cloud drone
+////////////////////////////////////
+drone.note(1)
+
+draw = function () {
+  webgl.clear();
+  image(clouds, width/2, height/2, width, height);
+  pg.clear();
+  pg.drawingContext.drawImage(hydra.canvas, 0, 0, pg.width, pg.height);
+  webgl.texture(pg);
+  webgl.rotateX(0.002);
+  webgl.rotateZ(0.003);
+  webgl.box(height / 2);
+  image(webgl, width/2, height/2);
+};
+canvas.style.display = "none";
+osc(6, 0.03, 2).out(o1);
+src(s0).out(o2);
+src(o0).modulateHue(src(o0).scale(1.002).kaleid(10), 100).blend(src(o2), () => 1 - (spacetime.out(5))).blend(src(o1), 0.02).out();
+
+f = FM[5]("bass", { decay: 3, attack: 1, gain: .1 }).connect(d38, 0.3).connect(r, 1)
+f.note.seq([4, 0, -3, 4, 11], [3, 2, 1/8].rnd());
+
+s = Synth("bleep", { decay: 1/8, attack: 1/24, glide: 2000 }).connect(d38, 0.7);
+s.seq((pp = [0, 2, -8, 14]), [1 / 8, 1, 2, 1/16, 1/16].rnd());
+pp.transpose.seq([12, -12, -12, 12], 4);
+
+p = FM("perc").connect(d38, 0.2);
+p.gain.seq([0.1, 0.1, 0.7, 0.1].rnd(), 1/8);
+p.note.seq([10, 0], [1/8, 1/16, 1/8, 1/8, 1/16], 1);
+
+spacetime.note(-1);
+
+canvas.style.display = "block";
+src(s1).scale(0.5).kaleid(3).out(o2)
+src(o2).modulateHue(src(o2).scale(1.2), 100).blend(src(o2), () => .8 - (spacetime.out(78))).blend(src(o2), 0.6).out();
+
+////////////////////////////////////
+//09 chromatic aberration dream
+////////////////////////////////////
+loop92.note.seq(1, 4, 0)
+drone.fadeout()
+spacetime.gain = 0
+f.stop()
+p.stop()
+words = ['EXTASY', 'DREAM', 'ETERNITY']
+wordNo = 0
+resetHydra()
+webgl = createGraphics(width, height, WEBGL);
+theShader = webgl.createShader(vertShader, fragShader)
+tex = createGraphics(width, height)
+tex.imageMode(CENTER)
+strength = .1
+draw = function() {
+    if(frameCount%200 == 0){
+      wordNo = rndi(2)
+    }
+    sound = (loop92.out(strength,0.1,8000) + loop93.out(strength,0.1,8000)) * 0.1
+    if(frameCount%10 == 0){
+	    tex.noFill()
+	    tex.stroke(255)
+	    let y = random(tex.height)
+	    tex.image(genitals[rndi(8)],random(width), random(height), 550, 550)
+	    tex.fill(255)
+	    tex.textSize(tex.width / 5)
+	    tex.textAlign(CENTER, CENTER)
+	    tex.text(words[wordNo], tex.width / 2, tex.height / 2)
+	}  
+	theShader.setUniform("resolution", [width, height])
+	theShader.setUniform('tInput', tex)
+	theShader.setUniform("max_distort", abs(sin(frameCount * .001) * 5))
+	theShader.setUniform("iMouse", [0.5 + sin(frameCount * .01 * map(sound, 0, .2, 1, 0)) * .3, .5 + cos(frameCount * .1) * map(sound, 0, .2, 0, 1)])
+	theShader.setUniform("num_iter", 12)
+	theShader.setUniform("focalDistance", 42.0)
+	theShader.setUniform("aperture", 12.0)
+ 
+	webgl.shader(theShader) // apply shader
+	webgl.rect(0, 0, width, height) // display shader
+    image(webgl,width/2,height/2)
+}
+
+strength = .2
+loop92.note.seq(1, 4, 0)
+loop93.note.seq(1, 4)
+
+loop92.fadeout()
+loop93.fadeout()
+
+////////////////////////////////////
+//10 interstellar worm
+////////////////////////////////////
+f = FM[5]("frog", {decay: 1/4, cmRatio: .9}).connect(r,.5)
+f.gain.fade(0.5,1,24)
+f.note.seq([-10, -5, 2, -20, -13], [1,1/2, 1/8].rnd());
+stroke(0)
+
+draw = function () {
+    x = map(noise(frameCount * .004),0,1,-200, width+300)
+    y = map(noise(frameCount * .006),0,1,-200, width+300)
+    translate(x, y);
+    fill(255)
+    b = f.out(200);
+    rect(b * -0.5, b * -0.5, b, b);
+  }
+
+//colorful shapeshifting worm
+draw = function () {
+  speedX = sin(frameCount * .011)
+  speedY = sin(frameCount * .028)
+  
+  x = map(noise(frameCount * .004),0,1,-200, width+300)
+	y = map(noise(frameCount * .006),0,1,-200, width+300)
+  translate(x, y);
+  const b = 3 + f.out(150);
+  if(frameCount%2000>1000){
+    f.cmRatio = .99
+  	ellipse(0,0,b);
+  	fill(100+200*speedX, f.out(1000), 100+200*speedY)  
+  }
+  else{
+    f.cmRatio = .2
+  	fill(f.out(300))  
+    rect(b * -0.5, b * -0.5, b, b);
+  }
+};
+
+////////////////////////////////////
+//11 grinder doomhole
+////////////////////////////////////
+s2.initScreen();
+losing1.trigger(1)
+f.stop()
+resetHydra()
+s = Synth('blank', { waveform:'square' }).note.seq( 12, 1 )
+s.connect(r, 1).connect(d38, .1)
+src(s2).kaleid(2).out();
+pg.reset()
+webgl.reset()
+webgl.noStroke()
+webgl.rotateY(1)
+webgl.translate(500,0,0)
+direction = -3
+counter = 0
+draw = function () {
+  counter++
+  if(counter%480==0){
+    direction *= -1
+  }
+  webgl.clear();
+  pg.drawingContext.drawImage(hydra.canvas, 0, 0, pg.width, pg.height);
+  if(s.out()>.2){
+		pg.image(grindr[rndi(grindr.length-1)], width/2, height/2)
+  }
+  webgl.texture(pg);
+  
+  webgl.box(width, height)
+  webgl.rotateX(0.01)
+  webgl.translate(direction,0,0)
+  
+  image(webgl, width/2, height/2)
+};
+
+losing2.trigger(1)
+
+losing1.fadeout()
+losing2.fadeout()
+
+///////////////////////////////////
+//12 acid glitch
+////////////////////////////////////
+acid.note.seq(1,1)
+hh = Hat({ gain: 0.1, tune: 0.7 }).connect(r, .3)
+canvas.style.display = "none";
+mod = .1
+draw = function () {
+    glitch.loadImage(smiley)
+  	glitch.replaceByte(acid.out(7500), 154)
+  	glitch.buildImage()
+  	image(glitch.image, width/4, height/2, height, height);
+  	image(glitch.image, width-width/4, height/2, height, height);  
+    image(smiley, rndi(width), rndi(height), 1+hh.out(1000), 1+hh.out(1000))
+};
+src(s0).modulate(src(s1),mod).out();
+
+hh.gain.seq([0.2, 0.2, 0.2, 0.2, 0.4, 0.2].rnd(), 1/16);
+hh.trigger.seq(1, 1/16);
+hh.fx.add(dS);
+dS.time = 1/10000;
+
+s = Synth('chirp', { decay: 1/16, gain: 1 }).connect(r, .5)
+s.loudness.seq([1, 0.5, 0.5, 1, 0.05, 0.2, 1, 0.1], 1/16)
+s.note.seq([0, 2, 4,], 1/16)
+
+mod = .1
+
+relax2.connect(d13, .3)
+relax.note(1)
+
+relax2.note.seq(1.2, 2)
+relax3.note.seq(1.4, 2, 0, 1)
+relax4.note.seq(1.5, 2, 0, 1.5)
+
+relax2.stop()
+
+s = Sine({frequency: 100, gain:.5}).connect()
+s.frequency.fade(10,200,32)
+
+////////////////////////////////////
+//13 changes
+////////////////////////////////////
+acid.rate.fade(1,0,16)
+hh.stop()
+s.frequency.fade(200,0,2)
+
+changes.note(1)
+
+acid.stop()
+resetHydra()
+webgl.reset()
+webgl.noStroke()
+flowerNo = 0
+hdriNo = 0
+soundNo = 0
+ambiences[soundNo].note(1)
+draw = function(){
+	if(frameCount%200==0){
+	  flowerNo = rndi(flowers.length-1)
+	}
+	if(frameCount%700==0){
+	  ambiences[soundNo].gain.fade(1,0,1)
+	  hdriNo = soundNo = soundNo + 1
+	  if(soundNo>8){ soundNo = 0; hdriNo = 0 }
+		ambiences[soundNo].note(1)
+    ambiences[soundNo].gain.fade(0,1,1)
+	}
+	webgl.clear()
+	webgl.texture(hdri[hdriNo])
+	webgl.sphere(1000)
+  
+	webgl.rotateX(changes.out(.01))
+ 	webgl.rotateY(changes.out(.02) + ambiences[soundNo].out(.1))
+	
+	webgl.push()
+	webgl.texture(flowers[flowerNo])
+	webgl.translate(0, 0, 0)
+	webgl.box(350)	
+	webgl.pop()
+	
+	image(webgl, width/2, height/2)
+}
+
+src(s0).modulate(src(s1),0).out();
+
+webgl.rotateX(1)
+
+////////////////////////////////////
+//14 technosexual
+////////////////////////////////////
+div = 1
+vid = loadVideo("http://127.0.0.1:8080/technosexual/vid" + 1 + ".mp4")
+texts = ['PRODUCE', 'CUM', 'FUCK', 'PROGRESS', 'MACHINE']
+iText = 0
+webgl.reset()
+fill(255,0,0)
+pg.fill(255,0,0)
+pg.imageMode(CORNER)
+currentVideo = 1
+draw = function(){  
+  if(frameCount%200 == 0){
+    currentVideo += 1
+    if(currentVideo > 6) currentVideo = 0
+    vid = loadVideo("http://127.0.0.1:8080/technosexual/vid" + currentVideo + ".mp4")
+  }
+  
+  for(i=0;i<div*div;i++){
+    rows = div
+    row = int(i/rows)
+    column = int(i%rows)
+    pg.image(vid, width/div*column, height/div*row, width/div, height/div);
+  }
+  pixelEffect(vid, true)
+  if(k.out()>.7){
+    iText += 1
+    if(iText>texts.length-1){
+      iText = 0
+    }
+  }
+	pg.text(texts[iText], pg.width/2, pg.height/2)
+  
+  webgl.clear()
+  webgl.texture(pg)
+  
+  webgl.push()
+  webgl.translate(width/4, height/4, 0)
+  webgl.rotateY(frameCount/50)
+  webgl.box(width/8)
+  webgl.pop()
+  
+  webgl.push()
+  webgl.translate(-width/4, height/4, 0)
+  webgl.rotateX(frameCount/50)
+  webgl.box(width/8)
+  webgl.pop()
+  
+  webgl.push()
+  webgl.translate(width/4, -height/4, 0)
+  webgl.rotateX(frameCount/50)
+  webgl.box(width/8)
+  webgl.pop()
+  
+  webgl.push()
+  webgl.translate(-width/4, -height/4, 0)
+  webgl.rotateX(frameCount/50)
+  webgl.box(width/8)
+  webgl.pop()
+  
+  webgl.rotateY(0.01)
+  
+  image(pg,width/2,height/2)
+  image(webgl,width/2, height/2)
+  
+	text(texts[iText], pg.width/2, pg.height/2)
+}
+k.note.seq( 1, 1/4 )
+hh = Hat()
+
+productive.note(.9)
+
+hh.trigger.seq( 1, 1/16 )
+
+sex.note(.8)
+
+technosexual.note(1)
+technosexual.fadeout()
+
+pix = 120
+div = 2
+
+k.note(1)
+
+rave.note.seq( 1, 4 )
